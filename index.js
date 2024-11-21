70eb048ed3d6cafb189c220d8fccc047a3306b1e
Rachael <schlosberg.r@northeastern.edu>
2024-11-21 18:26:44 -0500

    sever app changes

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 import express from 'express'
+import "dotenv/config";
+import session from "express-session";
+import cors from "cors";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+
+import UserRoutes from "./Kanbas/Users/routes.js";
 const app = express()
+ // make sure cors is used right after creating the app!!!
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
+app.use(express.json());
+UserRoutes(app);
+CourseRoutes(app);
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT || 4000)
